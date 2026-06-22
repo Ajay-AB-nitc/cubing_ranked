0684c716cef3a0996d38b47983ca27a3c62d17f1
Ajay-AB <ajay_b240124cs@nitc.ac.in>
2026-06-22 06:50:08 +0530

    sticker based rendering

--- app/Scene.tsx
+++ app/Scene.tsx
@@ -1,43 +1,4 @@
 "use client"
-// import { CubeCamera, OrbitControls, useHelper } from "@react-three/drei";
-// import { Canvas, useFrame } from "@react-three/fiber";
-// export function Cubie({position}: {position: [number, number, number]}){
-//     const colors = ["red", "orange", "white", "yellow", "green", "blue"] 
-//     return(
-//         <mesh position={position}>
-//             <boxGeometry args={[.9,.9,.9]}/>
-//             {colors.map((color, i) => (
-//                 <meshStandardMaterial key={i} attach={`material-${i}`} color={color}/>
-//             ))}
-//         </mesh>
-
-//     );
-// }
-
-
-// export default function Scene(){
-//     const positions:[number, number, number][] = []
-//     for (let i = -1; i <= 1; i++){
-//         for (let j = -1; j <= 1; j++){
-//             for (let k = -1; k <= 1; k++){
-//                positions.push([i,j,k]) 
-//             }
-//         }
-//     }
-//     return(
-//         <Canvas className="border-red-500">
-//             <ambientLight/>
-//             {/* <directionalLight position={[5,5,5]}/> */}
-//             {
-//                 positions.map(
-//                     (pos, i) => <Cubie key={i} position={pos}/>
-//                 )
-//             }
-//             {/* <Cubie position={0,0,0}></Cubie> */}
-//             <OrbitControls/>
-//         </Canvas>
-//     )
-// }
 
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
--- app/test.tsx
+++ app/test.tsx
@@ -1,74 +1,102 @@
-
-
-
-const cornerPositions = {
-    URF: [1,1,1],
-    UFL: [-1,1,1],
-    ULB: [-1,1,-1],
-    UBR: [1,1,-1],
-    DFR: [1,-1,1],
-    DLF: [-1,-1,1],
-    DBL: [-1,-1,-1],
-    DRB: [1,-1,-1],
+type RenderCubie = {
+    id: string
+    position: [number, number, number]
+    stickers: {
+        U?: string
+        D?: string
+        L?: string
+        R?: string
+        F?: string
+        B?: string
+    }
 }
 
-const edgePositions = {
-  UR: [1, 1, 0],
-  UF: [0, 1, 1],
-  UL: [-1, 1, 0],
-  UB: [0, 1, -1],
+const cubies: RenderCubie[] = [
+    // Corners
+    {
+        id: "URF",
+        position: [1, 1, 1],
+        stickers: { U: "white", R: "red", F: "green" }
+    },
+    {
+        id: "UFL",
+        position: [-1, 1, 1],
+        stickers: { U: "white", F: "green", L: "orange" }
+    },
+    {
+        id: "ULB",
+        position: [-1, 1, -1],
+        stickers: { U: "white", L: "orange", B: "blue" }
+    },
+    {
+        id: "UBR",
+        position: [1, 1, -1],
+        stickers: { U: "white", B: "blue", R: "red" }
+    },
+    {
+        id: "DFR",
+        position: [1, -1, 1],
+        stickers: { D: "yellow", F: "green", R: "red" }
+    },
+    {
+        id: "DLF",
+        position: [-1, -1, 1],
+        stickers: { D: "yellow", L: "orange", F: "green" }
+    },
+    {
+        id: "DBL",
+        position: [-1, -1, -1],
+        stickers: { D: "yellow", B: "blue", L: "orange" }
+    },
+    {
+        id: "DRB",
+        position: [1, -1, -1],
+        stickers: { D: "yellow", R: "red", B: "blue" }
+    },
 
-  DR: [1, -1, 0],
-  DF: [0, -1, 1],
-  DL: [-1, -1, 0],
-  DB: [0, -1, -1],
+    // Edges
+    { id: "UR", position: [1, 1, 0], stickers: { U: "white", R: "red" } },
+    { id: "UF", position: [0, 1, 1], stickers: { U: "white", F: "green" } },
+    { id: "UL", position: [-1, 1, 0], stickers: { U: "white", L: "orange" } },
+    { id: "UB", position: [0, 1, -1], stickers: { U: "white", B: "blue" } },
 
-  FR: [1, 0, 1],
-  FL: [-1, 0, 1],
-  BL: [-1, 0, -1],
-  BR: [1, 0, -1],
-} as const;
+    { id: "DR", position: [1, -1, 0], stickers: { D: "yellow", R: "red" } },
+    { id: "DF", position: [0, -1, 1], stickers: { D: "yellow", F: "green" } },
+    { id: "DL", position: [-1, -1, 0], stickers: { D: "yellow", L: "orange" } },
+    { id: "DB", position: [0, -1, -1], stickers: { D: "yellow", B: "blue" } },
 
-const centers = {
-  U: [0, 1, 0],
-  D: [0, -1, 0],
-  L: [-1, 0, 0],
-  R: [1, 0, 0],
-  F: [0, 0, 1],
-  B: [0, 0, -1],
-} as const;
+    { id: "FR", position: [1, 0, 1], stickers: { F: "green", R: "red" } },
+    { id: "FL", position: [-1, 0, 1], stickers: { F: "green", L: "orange" } },
+    { id: "BL", position: [-1, 0, -1], stickers: { B: "blue", L: "orange" } },
+    { id: "BR", position: [1, 0, -1], stickers: { B: "blue", R: "red" } },
 
-const cubies = [
-  ...Object.entries(cornerPositions).map(([id, position]) => ({
-    id,
-    position,
-  })),
-  ...Object.entries(edgePositions).map(([id, position]) => ({
-    id,
-    position,
-  })),
-  ...Object.entries(centers).map(([id, position]) => ({
-    id,
-    position,
-  })),
-];
+    // Centers
+    { id: "U", position: [0, 1, 0], stickers: { U: "white" } },
+    { id: "D", position: [0, -1, 0], stickers: { D: "yellow" } },
+    { id: "L", position: [-1, 0, 0], stickers: { L: "orange" } },
+    { id: "R", position: [1, 0, 0], stickers: { R: "red" } },
+    { id: "F", position: [0, 0, 1], stickers: { F: "green" } },
+    { id: "B", position: [0, 0, -1], stickers: { B: "blue" } },
+]
 
-function Cubie({
-    position
-}: {
-    position: [number, number, number]
-}) {
+
+
+function Cubie({ cubie }: { cubie: RenderCubie }) {
+    const s = cubie.stickers
+
+    const materials = [
+        s.R ?? "black", // +X
+        s.L ?? "black", // -X
+        s.U ?? "black", // +Y
+        s.D ?? "black", // -Y
+        s.F ?? "black", // +Z
+        s.B ?? "black", // -Z
+    ]
+
     return (
-        <mesh position={position}>
-            <boxGeometry args={[0.95,0.95,0.95]} />
-            {[
-                "red",
-                "orange",
-                "white",
-                "yellow",
-                "green",
-                "blue"
-            ].map((color, i) => (
+        <mesh position={cubie.position}>
+            <boxGeometry args={[0.95, 0.95, 0.95]} />
+            {materials.map((color, i) => (
                 <meshStandardMaterial
                     key={i}
                     attach={`material-${i}`}
@@ -80,14 +108,11 @@ function Cubie({
 }
 
 export default function RubiksCube() {
-  return (
-    <>
-      {cubies.map((c) => (
-        <Cubie
-          key={c.id}
-          position={c.position as [number, number, number]}
-        />
-      ))}
-    </>
-  );
+    return (
+        <>
+            {cubies.map((cubie) => (
+                <Cubie key={cubie.id} cubie={cubie} />
+            ))}
+        </>
+    )
 }
